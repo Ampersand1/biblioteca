d96ce6d4e99a1612edeed2484c3c1b49789b1e5b
fsalamanca2000 <juanf.salamancag@konradlorenz.edu.co>
2024-11-05 21:02:20 -0500

    Metodo PUT

--- src/routes/inventario.js
+++ src/routes/inventario.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const userSchema = require("../models/inventario");
+const inventarioSchema = require("../models/inventario");
+const inventario = require("../models/inventario");
 
 // Método para obtener todos los libros -SOLO PARA EL DESARROLLO-
 router.get("/inventario", (req, res) => {
@@ -18,11 +19,11 @@ router.get("/inventario/buscar", verifyAdmin, verifyToken, async (req, res) => {
             $or: [{ nombre }, { autor }]
         });
 
-        if (!user) {
+        if (!inventario) {
             return res.status(404).json({ message: "Libro u articulo no encontrado" });
         }
 
-        res.json(user);
+        res.json(inventario);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -34,6 +35,31 @@ router.post("/inventario",verifyAdmin, verifyToken, (req, res) => {
         .save()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
+});
+// Método para cambiar información de la referencia
+router.put("/inventario", verifyAdmin, verifyToken, async (req, res) => {
+    const { Nombre, GeneroPrincipal, GeneroSecundario, Autor, AñoPubli, Editorial, ISBN } = req.body;
+
+    try {
+        // Usamos el id del usuario desde el token
+        const inventario = await inventarioSchema.findById(req.libro.id);
+        if (!inventario) {
+            return res.status(404).json({ error: "Referencia no encontrada" });
+        }
+        // Actualizamos los campos solo si se envían en la solicitud
+        if (Nombre) inventario.Nombre = Nombre;
+        if (GeneroPrincipal) inventario.GeneroPrincipal = GeneroPrincipal;
+        if (GeneroSecundario) inventario.GeneroSecundario = GeneroSecundario;
+        if (Autor) inventario.Autor = Autor;
+        if (AñoPubli) inventario.AñoPubli = AñoPubli;
+        if (Editorial) inventario.Editorial = Editorial;
+        if (ISBN) inventario.ISBN = ISBN;
+        // Guardamos los cambios en la base de datos
+        await inventario.save();
+        res.json({ message: "Datos del libro en el inventario actualizados con éxito", inventario });
+    } catch (error) {
+        res.status(500).json({ error: "Error al actualizar el inventario" });
+    }
 });
 // Método -SOLO DESARROLLO- para borrar a un libro por su ID 
 router.delete("/inventario/borrar/:id", async (req, res) => {
